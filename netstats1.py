d07e76934c42fb32cd9b3540a104edb6e039a7b8
stlsmoore <shawn.moore7395@gmail.com>
2020-08-17 15:51:01 -0500

    Update netstats1.py

--- netstats1.py
+++ netstats1.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug 12 20:45:17 2020
-
 @author: moore
 """
 import re
@@ -16,7 +15,7 @@ def sorter(netinfo):
     
     devPat = 'switch..'
     intPat = 'te...'
-    bpsPat = '\Ad+'
+    bpsPat = r'\b[0-9]\d+\b'
     
     devMatch = re.findall(devPat, netinfo)
     intMatch = re.findall(intPat, netinfo)
@@ -26,13 +25,13 @@ def sorter(netinfo):
     #print(intMatch)
     #print(bpsMatch)
     
-    for index in devMatch:
-        infoDict[index + ' inter'] = 'value'
-        
-    
+    for dev, int, bps in zip(devMatch, intMatch, bpsMatch):
+        infoDict['{}--{}'.format(dev, int)] = bps
 
-    #print(contents)        
-    print(infoDict)
+    newDict = dict(zip(infoDict.keys(), [float(value) for value in infoDict.values()]))
+	
     
+    for k,v in sorted(newDict.items(), key=lambda x:x[1]):
+        print(k, v)
 
 sorter(contents)
